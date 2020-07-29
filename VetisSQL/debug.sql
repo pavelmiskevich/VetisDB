b56577f12bba9c4324b32c059b1dbfca2454d8c4
pavelmiskevich <p_a_v_l_i_k@mail.ru>
2020-07-29 14:34:41 +0300

    debug

---
+++ VetisSQL/debug.sql
@@ -0,0 +1,7 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT * FROM [1gb_vetisdbtest].[dbo].[BusinessEntities]
+select count(*) FROM [1gb_vetisdbtest].[dbo].[BusinessEntities]
+select * from BusinessEntities where uuid = '04d76637-957d-41a8-8e73-fa53a6e5d78b'
+'051e0b1e-06e9-4d6e-8091-0ea2e0af8115'
+'067385be-447b-4abb-8fbd-e09bc12ead7e'
+'07c25802-d96c-49cc-8522-c0a0dca3fbc2'
